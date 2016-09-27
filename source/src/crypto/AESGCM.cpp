9ddb769c10f12f70e16e48911ba8a52d88ea9078
tom <dangxuanthong@gmail.com>
2016-09-27 16:56:14 +0200

    - add nist specs for aes-gcm implementation

--- source/src/crypto/AESGCM.cpp
+++ source/src/crypto/AESGCM.cpp
@@ -1,9 +1,22 @@
 #include "AESGCM.h"
 
-AESGCM::AESGCM(int keyLength){
-	this->keyLength = keyLength;	
+AESGCM::AESGCM(int keyLength){	
+	
+	if (keyLength<=0 || keyLength%128!=0){
+		throw ERROR_KEYLENGTH_NOT_SUPPORTED;
+	}
+	this->keyLength = keyLength;
+	this->key = (uint8_t*)malloc(keyLength>>3);		
+}
+
+void AESGCM::setKey(const uint8_t* key, enum SecurityParameter secuParam){
+	memcpy(this->key, key, this->keyLength);
+	this->secuParam = secuParam;
 }
 
 AESGCM::~AEGGCM(){
+	//--	securely remove the key by setting its value to 0
+	memset(this->key, 0, this->keyLength);
+	delete this->key;
 }
 
--- source/src/crypto/AESGCM.h
+++ source/src/crypto/AESGCM.h
@@ -3,21 +3,37 @@
 
 	#include "crypto-utils.h"
 	
+	#define ERROR_KEYLENGTH_NOT_SUPPORTED "Keylength is not supported"
+	
+	#define BLOCK_SIZE 128
+		
+	enum SecurityParameter : int {
+		SECU_128 = 128,
+		SECU_120 = 120,
+		SECU_112 = 112,
+		SECU_104 = 104,
+		SECU_96 = 96
+	};
+	
 	
 	class AESGCM{
-	
+					
 		uint8_t* key;
 		int keyLength;
+		enum SecurityParameter secuParam;
 		
 		private:
+			static void mulBlock();
 		
 		public:
+			
+			//--	SECURITY PARAMETERS			
 			AESGCM(int keyLength);
 			~AESGCM();
-			void setKey(uint8_t* key);
+			void setKey(const uint8_t* key, enum SecurityParameter secuParam);
 			bool encrypt(const uint8_t* data, uint8_t* encrypted);
 			bool decrypt(const uint8_t* encrypted, uint8_t* data);
-	}
+	};
 
 
 #endif
