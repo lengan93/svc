beffa2f8f2255acaf8bb0bacb399fb94683d5b9a
thongdx <dangxuanthong@gmail.com>
2017-05-20 18:37:25 +0700

    add testudp

---
+++ source/testudp/udpreceiver.cpp
@@ -0,0 +1,30 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <string>
+#include <unistd.h>
+#include <errno.h>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct sockaddr_in addr;
+	inet_pton(AF_INET, "0.0.0.0", &(addr.sin_addr));
+	addr.sin_port = htons(stoi("9293"));
+	addr.sin_family = AF_INET;
+
+	char buffer[1000];
+	memset(buffer, 0 , 1000);
+	if (bind(sock, (struct sockaddr*) &addr, sizeof(addr)) == 0){
+		printf("listening...\n");
+		recv(sock, buffer, 1000, 0);
+		printf("receiver: %s\n", buffer);
+		return 0;
+	}
+	else{
+		printf("bind failed, error: %d\n", errno);
+		return -1;
+	}
+}
---
+++ source/testudp/udpsender.cpp
@@ -0,0 +1,28 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <string>
+#include <unistd.h>
+#include <errno.h>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct sockaddr_in addr;
+	inet_pton(AF_INET, "0.0.0.0", &(addr.sin_addr));
+	addr.sin_port = htons(stoi("9293"));
+	addr.sin_family = AF_INET;
+	string data = "there is con khi";
+
+	if (connect(sock, (struct sockaddr*) &addr, sizeof(addr)) == 0){
+		send(sock, data.c_str(), strlen(data.c_str()), 0);
+		printf("sent");
+		return 0;
+	}
+	else{
+		printf("sent failed, error: %d\n", errno);
+		return -1;
+	}
+}
